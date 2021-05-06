e533d2bd450945312655fc3dd890fa9372492724
Aleksei Voronov <despawn@gmail.com>
2021-05-06 21:28:47 +0200

    Initial Commit

---
+++ Palach/PalachApp.swift
@@ -0,0 +1,17 @@
+//
+//  PalachApp.swift
+//  Palach
+//
+//  Created by Aleksei on 06/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct PalachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
